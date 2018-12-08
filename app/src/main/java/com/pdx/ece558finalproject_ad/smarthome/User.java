99067f8014e8beea3807063cc6a0bf45e7102ba5
athmahi <atharvamahindrakar@gmail.com>
2018-12-07 22:20:54 -0800

    Final Commit by Atharva Mahindrakar

--- app/src/main/java/com/pdx/ece558finalproject_ad/smarthome/User.java
+++ app/src/main/java/com/pdx/ece558finalproject_ad/smarthome/User.java
@@ -12,90 +12,183 @@ public class User {
     private String MAC;
     private int RSSI;
 
+    /**
+     *
+     * @return SSID
+     */
     public String getSSID() {
         return SSID;
     }
 
+    /**
+     * set SSID
+     * @param SSID
+     */
     public void setSSID(String SSID) {
         this.SSID = SSID;
     }
 
+    /**
+     *
+     * @return MAC
+     */
     public String getMAC() {
         return MAC;
     }
+
+    /**
+     * Set MAC
+     * @param MAC
+     */
 
     public void setMAC(String MAC) {
         this.MAC = MAC;
     }
 
+    /**
+     *
+     * @return RSSI
+     */
     public int getRSSI() {
         return RSSI;
     }
 
+    /**
+     * set RSSI
+     * @param RSSI
+     */
     public void setRSSI(int RSSI) {
         this.RSSI = RSSI;
     }
+
+
 
     public User(){
 
     }
 
+    /**
+     *
+     * @return email
+     */
     public String getEmail() {
         return email;
     }
+
+    /**
+     * set email
+     * @param email
+     */
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     *
+     * @return mnetworkInfor
+     */
     public NetworkInformation getNetworkInfo() {
         return mNetworkInfo;
     }
 
+    /**
+     * sets networkInfor
+     * @param networkInfo
+     * @return object
+     */
     public User setNetworkInfo(NetworkInformation networkInfo) {
         this.mNetworkInfo = networkInfo;
         return this;
     }
+
+    /**
+     * sets userid and password
+     * @param userId
+     * @param passWord
+     */
 
     public User(String userId, String passWord) {
         this.userId = userId;
         this.mPassword = passWord;
     }
 
+    /**
+     *
+     * @param userId
+     * @param passWord
+     * @param networkInfo
+     */
     public User(String userId, String passWord, NetworkInformation networkInfo){
         this.userId = userId;
         this.mPassword = passWord;
         this.mNetworkInfo = networkInfo;
     }
 
+    /**
+     *
+     * @return latitude
+     */
     public double getLatitude() {
         return latitude;
     }
+
+    /**
+     * set latitude
+     * @param latitude
+     */
 
     public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
 
+
+    /**
+     *
+     * @return longitude
+     */
     public double getLongitude() {
         return longitude;
     }
 
+    /**
+     * sets longitude
+     * @param longitude
+     */
     public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
 
+    /**
+     *
+     * @return userid
+     */
     public String getUserId() {
         return userId;
     }
+
+    /**
+     * sets userid
+     * @param userId
+     */
 
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
+    /**
+     *
+     * @return password
+     */
+
     public String getPassword() {
         return mPassword;
     }
 
+    /**
+     * sets password
+     * @param password
+     */
     public void setPassword(String password) {
         this.mPassword = password;
     }
